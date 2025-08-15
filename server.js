35960fd378788c1d9f6f9b728873998686aef21d
tushargithub52 <tusharraiku6020@gmail.com>
2025-08-15 17:03:58 +0530

    feat: add dotenv configuration and implement user prompt handling in server

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
+require('dotenv').config();
 const app = require('./src/app');
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const { generateResponse } = require('./src/service/ai.service')
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
 io.on("connection", (socket) => {
-  // ...
+    console.log("A user connected");
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+
+    socket.on("user-prompt", async (data) => {
+
+        console.log("Received user prompt:", data.prompt);
+        try {
+            const response = await generateResponse(data.prompt);
+            console.log("Generated response:", response);
+            socket.emit("bot-response", { response });
+        } catch (error) {
+            console.error("Error generating response:", error);
+            socket.emit("bot-response", { response: "Sorry, I couldn't process your request." });
+        }
+
+    })
+
 });
 
 httpServer.listen(3000, () => {
