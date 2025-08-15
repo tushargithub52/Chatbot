767f0e118b57305808f8d1882148aa4d1d87072d
tushargithub52 <tusharraiku6020@gmail.com>
2025-08-15 15:29:47 +0530

    feat: initialize chatbot project with Express and Socket.io

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require('./src/app');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+  // ...
+});
+
+httpServer.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
